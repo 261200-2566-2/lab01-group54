4402a68a3fad452d53199af36110a5ab11da36e5
monophoto <jetsadawut1013@gmail.com>
2023-11-22 23:09:30 +0700

    edit-new print n per line.

--- src/Lab01.java
+++ src/Lab01.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 public class Lab01 {
 
-    public static void sortedandprint(String input) {
+    public static void sortedandprint(int input) {
         try {
-            String[] splited_input = input.split(" ");
-            int[] arr = Stream.of(splited_input).mapToInt(Integer::parseInt).toArray();
-            int temp = arr[0];
-            if(arr[0] >= arr.length) {
-                temp = arr.length - 1;
+            Scanner scanner = new Scanner(System.in);
+
+            int[] integer = new int[input];
+
+            for(int i = 0; i < input; i++){
+                integer[i] = scanner.nextInt();
             }
-            Arrays.sort(arr,1, arr.length);
-//            System.out.println(Arrays.toString(arr));
-            for (int i = 1; i <= temp; i++) {
-                System.out.print(arr[i] + " ");
+            Arrays.sort(integer);
+
+            for (int i = 0; i < input; i++){
+                System.out.println(integer[i]);
             }
+
+            scanner.close();
+
+
         }catch (NumberFormatException e) {
             System.out.println("Bad input!!");
         }
@@ -27,7 +31,7 @@ public class Lab01 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter your num : ");
-        sortedandprint(s.nextLine());
+        sortedandprint(s.nextInt());
 
 
     }
